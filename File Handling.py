1dfe8cba890e591e929b88b8058ccacc0cad1e12
Arka Bhattacharya <rehanarka07@gmail.com>
2026-06-30 14:21:04 +0530

    Add files via upload

---
+++ File Handling.py
@@ -0,0 +1,22 @@
+# file = open("file.txt", "w")
+# w = file.write("Thank You")
+# print(w)
+# file.close()
+
+# file = open("/Users/arkabhattacharya/Downloads/Backrooms/Player.py")
+# r = file.read()
+# print(r)
+# file.close()
+
+# txt = "I like mangoes"
+# print(txt)
+# x = txt.replace("mangoes", "Oranges")
+# print(x)
+
+# text = "Oranges"
+# print(text)
+# y = text.strip()
+# print("I love eating", y ,"inside my house")
+
+# f = open("file.txt", "r")
+# print(f.readlines())
